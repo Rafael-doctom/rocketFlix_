1768a8bcc577af618f55bb29a7c560e1b9ebe972
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-04-11 10:31:33 -0300

    feat: api key added

---
+++ api.js
@@ -0,0 +1,4 @@
+const BASE_URL = 'https://api.themoviedb.org/3/movie/';
+const API_KEY = 'd54bd45f38aa4b1f75c30a3086f27e83';
+const ID_EXAMPLE = '550';
+const BASE_API_EXAMPLE =  `${BASE_URL}${ID_EXAMPLE}?api_key=${API_KEY}`;

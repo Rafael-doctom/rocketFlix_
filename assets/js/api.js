5828710a6dc1e2522a33f321f40c7c7bce54fcd7
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-04-11 11:18:25 -0300

    feat: base code added with styles & reset

---
+++ assets/js/api.js
@@ -0,0 +1,6 @@
+const BASE_URL = 'https://api.themoviedb.org/3/movie/';
+const API_KEY = 'd54bd45f38aa4b1f75c30a3086f27e83';
+const ID_EXAMPLE = '675353';
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
+const LANGUAGE = 'language=PT-BR';
+const BASE_API_EXAMPLE =  `${BASE_URL}${ID_EXAMPLE}?api_key=${API_KEY}&${BASE_API_EXAMPLE}`;
